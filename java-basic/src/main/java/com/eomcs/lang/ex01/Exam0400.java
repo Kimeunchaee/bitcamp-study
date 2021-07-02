a327d2fd2fab575aa0ef652fbaf3ef727ce97a77
Kimeunchaee <eun00218@naver.com>
2021-07-02 18:06:47 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex01/Exam0400.java
+++ java-basic/src/main/java/com/eomcs/lang/ex01/Exam0400.java
@@ -1,17 +1,31 @@
 package com.eomcs.lang.ex01;
 
-// main() 메소드의 내용은 뒤에 있는 {}이다
-// {} 안의 내용을 컴파일(javac)후 실행(java)시킬때
-// JVM이 main() 메소드의 내용을 실행시키기때문에
-// main()메소드가 없으면 오류가 발생한다
-// main()메소드는 필수로 작성!
+//# main() 메서드
+//- 다음과 같이 JVM을 통해 클래스를 실행하면, JVM은 그 클래스에서 main() 메서드를 찾아 실행한다.
+//  예) $ java -cp bin/main com.eomcs.basic.ex01.Exam4
+//- main() 메서드가 없으면 실행 오류가 발생한다.
+//
+//## JVM이 클래스를 실행하는 과정
+//1) 실행할 클래스 파일을 CLASSPATH에 등록된 디렉토리 경로에서 찾는다.
+//2) 클래스 파일이 유효한 바이트코드(bytecode) 인지 검사한다.
+//3) 메모리에 바이트코드를 적재(load)한다.
+//4) public static void main(String[] args) {} 블록을 찾는다.
+//5) main() 블록에 들어 있는 코드를 실행한다.
+//
 
-
-public class Exam0400{
-    public static void main(String[] args){
-
-    }
+public class Exam0400 {
+  
+  public static void main(String[] args) {
+  }
+  
 }
 
-//JVM 실행 : java -cp bin/main com.eomcs.lang.ex01.Exam0400
-// 입력하면 결과가 보임 (공백)
+//## 실습
+//1) 컴파일하기
+//- $ javac -d bin/main -encoding UTF-8 src/main/java/com/eomcs/basic/ex01/Exam4.java
+//
+//2) 실행하기
+//- $ java -cp bin/main com.eomcs.basic.ex01.Exam4
+//
+//* main() 메서드를 주석으로 막은 다음에 컴파일하고 실행해 보자.
+//- 실행 오류 메시지를 확인할 수 있다.
