479077c266259c5af580e5c4cde59b384ca095bd
Kimeunchaee <eun00218@naver.com>
2021-07-06 12:43:10 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex03/Exam0310.java
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0310.java
@@ -7,18 +7,25 @@ package com.eomcs.lang.ex03;
 //
 public class Exam0310 {
   public static void main(String[] args) {
-    
+
     //## 10진수 표기법
     System.out.println(3.14);
-    
+
     //## Exponential 기호 사용하기
     //- e 기호를 사용하면 소수점의 위치를 조정할 수 있다.
     //- 소수점의 위치를 움직인다고 해서 "부동소수점(floating point)"이라 부른다. 
     //- 표기법
     //    숫자e지수 또는 숫자E지수 
+
+    /*
     System.out.println(0.0314e2); // 0.0314 * 10의2승 = 3.14
     System.out.println(0.314e1); // 0.314 * 10의1승 = 3.14
     System.out.println(31.4e-1); // 31.4 * 10의-1승 = 3.14
     System.out.println(314e-2); // 314 * 10의-2승 = 3.14
+     */
+
+    System.out.println(0.00314e3);
+    System.out.println(314.00e-2);
+    System.out.println(35e-4);
   }
 }
