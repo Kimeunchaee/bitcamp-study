5e502c7e016af54a15111dc25836795b83711478
Kimeunchaee <eun00218@naver.com>
2021-07-08 14:34:55 +0900

    .

---
+++ java-basic/src/main/java/com/eomcs/basic/ex10/Exam0100.java
@@ -0,0 +1,18 @@
+package com.eomcs.basic.ex10;
+
+public class Exam0100 {
+  public static void main(String[] args) {
+    long mills = System.currentTimeMillis();
+    //double 은 8바이트지만 지수부 가수부 나눠서 표현하기때문에 16자리만 표현되고
+    // 숫자가 짤려서 표현됨 > 너무 큰 수 이기때문에 눈에 안보임 , 오류는 안뜨지만
+    // doble은 잘못된 변수임
+
+    System.out.println(mills);
+    //java.util.Date d = new java.util.Date(mills); // Date 객체을 만드는 순간의 시각을 저장해둔다
+    java.sql.Date d = new java.sql.Date(mills);
+    System.out.println(d.toString());
+    System.out.printf("%tY-%tm-%td", d, d, d);
+    System.out.printf("%tY-%1$tm-%1$td\n", d);
+  }
+
+}
--- java-basic/src/main/java/com/eomcs/lang/ex04/Exam0510.java
+++ java-basic/src/main/java/com/eomcs/lang/ex04/Exam0510.java
@@ -1,12 +1,15 @@
 package com.eomcs.lang.ex04;
 
 //# 배열 - 배열을 선언하는 방법 
-// 배열?
-// - 같은 종류의 메모리를 쉽게 만드는 방법
+// 배열? : 같은 종류의 메모리를 쉽게 만드는 방법
 //
 // - 문법
 //        메모리종류[] 메모리이름 = new 메모리종류[개수]
 //        데이터타입[] 변수명 = new 데이터타입[개수];
+//        ex) int 자료형의 배열은 int[] 로 표현
+//            String 타입의 배열은 String[]로 표현
+//        
+//        - 즉 배열이란 자료형의 종류가 아닌 자료형의 집합을 의미한다!
 //        ex) int[] arr = new int[5];
 //
 // - C언어 스타일 
@@ -22,22 +25,24 @@ public class Exam0510 {
     int i1, i2, i3, i4, i5;
 
     // 배열 선언
+    // 배열의 길이를 설정하여 변수를 먼저 새앙하고 값을 나중에 대입하는 방식
+    // 반드시 길이값이 필요하며, 길이값이 없으면 오류가 발생한다
     int[] arr1 = new int[5]; // OK!
     int arr2[] = new int[5]; // OK! C-style.
 
     // 배열의 크기는 int 타입의 최대 값에서 2를 뺀 값이다.
     // 배열의 최대 크기 = Integer.MAX_VALUE - 2
-    
+
     //int[] arr3 = new int[2147483647]; // 실행 오류 => VM의 배열 크기 제한을 초과
     // Exception in thread "main" java.lang.OutOfMemoryError: Requested array size exceeds VM limit
-    
+
     int[] arr3 = new int[2147483645]; // OK!
     //int[] arr3 = new int[Integer.MAX_VALUE - 2]; // OK!
 
     // 실행 오류 발생!
     // => Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
     // => JVM이 OS로부터 사용 허가를 받은 메모리 크기를 벗어났기 때문에 발생한 것이다.
-    
+
     // 해결책?
     // => JVM을 실행할 때 최대 힙(heap) 메모리의 크기를 늘리면 된다.
     // => JVM 실행 옵션에 다음을 추가하라!
