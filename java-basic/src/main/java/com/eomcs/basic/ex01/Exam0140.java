32b170c8c7cc7f306a1650bfb4e15bb3252fa9e7
Kimeunchaee <eun00218@naver.com>
2021-08-10 19:28:11 +0900

    .

--- java-basic/src/main/java/com/eomcs/basic/ex01/Exam0140.java
+++ java-basic/src/main/java/com/eomcs/basic/ex01/Exam0140.java
@@ -19,7 +19,8 @@ public class Exam0140 {
 
     // Object에서 상속 받은 hashCode()는 인스턴스마다 고유의 4바이트 정수 값을 리턴한다.
     // => 이 값은 toString()의 출력 값으로 사용된다.
-    System.out.println(Integer.toHexString(obj1.hashCode()));
+    System.out.println(Integer.toHexString(obj1.hashCode())); 
+    //10진수로 출력되는걸 16진수로 바꿔서 문자열로 리턴하도록 함
     System.out.println(Integer.toHexString(obj2.hashCode()));
 
     System.out.println(obj1);
