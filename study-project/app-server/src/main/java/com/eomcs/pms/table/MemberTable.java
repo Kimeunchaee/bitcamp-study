bdb66086c5b656bdcdeb00e7dc9c815491618287
Kimeunchaee <eun00218@naver.com>
2021-10-18 10:22:31 +0900

    1018 push 스터디 프로젝트 폴더 복사

--- study-project/app-server/src/main/java/com/eomcs/pms/table/MemberTable.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/table/MemberTable.java
@@ -5,6 +5,9 @@ import com.eomcs.server.DataProcessor;
 import com.eomcs.server.Request;
 import com.eomcs.server.Response;
 
+// 역할
+// - 회원 데이터를 처리하는 일을 한다.
+// 
 public class MemberTable extends JsonDataTable<Member> implements DataProcessor {
 
   public MemberTable() {
@@ -13,11 +16,17 @@ public class MemberTable extends JsonDataTable<Member> implements DataProcessor 
 
   @Override
   public void execute(Request request, Response response) throws Exception {
-    switch(request.getCommand()) {
+    switch (request.getCommand()) {
       case "member.insert": insert(request, response); break;
       case "member.selectList": selectList(request, response); break;
       case "member.selectOne": selectOne(request, response); break;
+      case "member.selectOneByEmailPassword": selectOneByEmailPassword(request, response); break;
+      case "member.selectOneByName": selectOneByName(request, response); break;
+      case "member.update": update(request, response); break;
       case "member.delete": delete(request, response); break;
+      default:
+        response.setStatus(Response.FAIL);
+        response.setValue("해당 명령을 지원하지 않습니다.");
     }
   }
 
@@ -35,6 +44,7 @@ public class MemberTable extends JsonDataTable<Member> implements DataProcessor 
   private void selectOne(Request request, Response response) throws Exception {
     int no = Integer.parseInt(request.getParameter("no"));
     Member m = findByNo(no);
+
     if (m != null) {
       response.setStatus(Response.SUCCESS);
       response.setValue(m);
@@ -42,6 +52,61 @@ public class MemberTable extends JsonDataTable<Member> implements DataProcessor 
       response.setStatus(Response.FAIL);
       response.setValue("해당 번호의 회원을 찾을 수 없습니다.");
     }
+  }
+
+  private void selectOneByEmailPassword(Request request, Response response) throws Exception {
+    String email = request.getParameter("email");
+    String password = request.getParameter("password");
+
+    Member member = null;
+    for (Member m : list) {
+      if (m.getEmail().equals(email) && m.getPassword().equals(password)) {
+        member = m;
+        break;
+      }
+    }
+
+    if (member != null) {
+      response.setStatus(Response.SUCCESS);
+      response.setValue(member);
+    } else {
+      response.setStatus(Response.FAIL);
+      response.setValue("해당 이메일과 암호를 가진 회원을 찾을 수 없습니다.");
+    }
+  }
+
+  private void selectOneByName(Request request, Response response) throws Exception {
+    String name = request.getParameter("name");
+    System.out.println("-----> " + name);
+    Member member = null;
+    for (Member m : list) {
+      if (m.getName().equals(name)) {
+        member = m;
+        break;
+      }
+    }
+
+    if (member != null) {
+      response.setStatus(Response.SUCCESS);
+      response.setValue(member);
+    } else {
+      response.setStatus(Response.FAIL);
+      response.setValue("해당 이름의 회원을 찾을 수 없습니다.");
+    }
+  }
+
+  private void update(Request request, Response response) throws Exception {
+    Member member = request.getObject(Member.class);
+
+    int index = indexOf(member.getNo());
+    if (index == -1) {
+      response.setStatus(Response.FAIL);
+      response.setValue("해당 번호의 회원을 찾을 수 없습니다.");
+      return;
+    }
+
+    list.set(index, member);
+    response.setStatus(Response.SUCCESS);
   }
 
   private void delete(Request request, Response response) throws Exception {
@@ -53,6 +118,7 @@ public class MemberTable extends JsonDataTable<Member> implements DataProcessor 
       response.setValue("해당 번호의 회원을 찾을 수 없습니다.");
       return;
     }
+
     list.remove(index);
     response.setStatus(Response.SUCCESS);
   }
@@ -66,17 +132,26 @@ public class MemberTable extends JsonDataTable<Member> implements DataProcessor 
     return null;
   }
 
-  private int indexOf(int no) {
+  private int indexOf(int memberNo) {
     for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).getNo() == no) {
+      if (list.get(i).getNo() == memberNo) {
         return i;
       }
     }
-    return -1; 
+    return -1;
   }
 
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
